97bd47636e33d9c4f71312facedec30b152fa38d
QuocHuwng <nguyenhung2952006@gmail.com>
2024-11-29 11:47:37 +0700

    Update Session08_Ex6.cpp

--- Session08_Ex6.cpp
+++ Session08_Ex6.cpp
@@ -2,9 +2,9 @@
 
 int main() {
     int arr[3][3] = {
-    	{1,2,3}, 
-		{4,5,6},
-		{7,8,9},
+    {1,2,3}, 
+    {4,5,6},
+    {7,8,9},
 	};
     int sum = 0;
     printf("Cac phan tu tren duong cheo chinh : ");
@@ -15,4 +15,3 @@ int main() {
     printf("\nTong cac phan tu : %d\n", sum);
     return 0;
 }
-
